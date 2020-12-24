1d45f2d3e9750582a6268faec44ddc686d1a2ac0
SaviorSeva <49302415+SaviorSeva@users.noreply.github.com>
2020-12-24 16:19:25 +0100

    Update register.c

--- registers.c
+++ registers.c
@@ -25,60 +25,189 @@ Contact: Guillaume.Huard@imag.fr
 #include <stdlib.h>
 
 struct registers_data {
-  uint32t reg;
-  int value;//?
-  uint8_t adr;//?
+    uint32_t reg_data;
 };
 
 registers registers_create() {
-    registers r = calloc(37,sizeof(uint32_t));
+    registers r = calloc(37, sizeof(uint32_t));
     return r;
 }
 
 void registers_destroy(registers r) {
+    free(r);
 }
 
 uint8_t get_mode(registers r) {
-    return 0;
-}
+    // modes:
+    // usr = 16
+    // fiq = 17
+    // irq = 18
+    // svc = 19
+    // abt = 23
+    // und = 27
+    // sys = 31
+    uint32_t cpsr = (r + 31)->reg_data;
+    uint8_t mode = cpsr & 0x1f;
+    return mode;
+} 
 
 int current_mode_has_spsr(registers r) {
-    return 0;
+    uint8_t mode = get_mode(r);
+    switch (mode){
+        case 16 :
+            return 0;
+            break;
+        case 31 :
+            return 0;
+            break;
+        default :
+            return 1;
+            break;
+    }
 }
 
 int in_a_privileged_mode(registers r) {
-    return 0;
+    uint8_t mode = get_mode(r);
+    switch (mode){
+        case 16 :
+            return 0;
+            break;
+        default :
+            return 1;
+            break;
+    }
 }
 
 uint32_t read_register(registers r, uint8_t reg) {
-    uint32_t value=0;
-    value=r->value;//?
-    return value;
+    if(reg < 0) return 0; 
+    if(reg < 8){
+        return (r + reg)->reg_data;
+    }
+    uint8_t mode = get_mode(r);
+    if(reg < 13){
+        switch (mode){
+            case 17 :
+                return (r + 9 + (reg-8)*2) ->reg_data;
+                break;
+            default :
+                return (r + 8 + (reg-8)*2) ->reg_data;
+                break;
+        }
+    }if(reg < 15){
+        switch (mode){
+            case 17 :
+                return (r + 23 + (reg-8)*2) -> reg_data;
+                break;
+            case 18 :
+                return (r + 22 + (reg-8)*2) -> reg_data;
+                break;
+            case 19 :
+                return (r + 19 + (reg-8)*2) -> reg_data;
+                break;
+            case 23 :
+                return (r + 20 + (reg-8)*2) -> reg_data;
+                break;
+            case 27 :
+                return (r + 21 + (reg-8)*2) -> reg_data;
+                break;
+            default :
+                return (r + 18 + (reg-13)*6) -> reg_data;
+                break;
+        }
+    }else{
+        return (r + 30) -> reg_data;
+    }
 }
 
 uint32_t read_usr_register(registers r, uint8_t reg) {
-    uint32_t value=0;
-    return value;
+    if(reg < 0) return 0; 
+    else if(reg < 8){
+        return (r + reg)->reg_data;
+    }else if(reg < 13){
+        return (r + 8 + (reg-8)*2) ->reg_data;
+    }else if(reg < 15){
+        return (r + 18 + (reg-13)*6) -> reg_data;
+    }else{
+        return (r + 30) -> reg_data;
+    }
 }
 
 uint32_t read_cpsr(registers r) {
-    uint32_t value=0;
-    return value;
+    return (r + 31) -> reg_data;
 }
 
 uint32_t read_spsr(registers r) {
-    uint32_t value=0;
-    return value;
+    return (r + 32) -> reg_data;
 }
 
 void write_register(registers r, uint8_t reg, uint32_t value) {
+    if(reg < 0) return;
+    uint8_t mode = get_mode(r);
+    if(reg < 8){
+        (r + reg) -> reg_data = value;
+        return;
+    }else if(reg < 13){
+        switch (mode){
+            case 17 :
+                (r + 9 + (reg-8)*2) -> reg_data = value;
+                return;
+                break;
+            default :
+                (r + 8 + (reg-8)*2) -> reg_data = value;
+                return;
+                break;
+        }
+    }else if(reg < 15){
+        switch (mode){
+            case 17 :
+                (r + 23 + (reg-8)*2) -> reg_data = value;
+                return ;
+                break;
+            case 18 :
+                (r + 22 + (reg-8)*2) -> reg_data = value;
+                return;
+                break;
+            case 19 :
+                (r + 19 + (reg-8)*2) -> reg_data = value;
+                return;
+                break;
+            case 23 :
+                (r + 20 + (reg-8)*2) -> reg_data = value;
+                return;
+                break;
+            case 27 :
+                (r + 21 + (reg-8)*2) -> reg_data = value;
+                return;
+                break;
+            default :
+                (r + 18 + (reg-13)*6) -> reg_data = value;
+                return;
+                break;
+        }
+    }else{
+        (r + 30) -> reg_data = value;
+        return;
+    }
 }
 
 void write_usr_register(registers r, uint8_t reg, uint32_t value) {
+    if(reg < 0) return;
+    uint8_t mode = get_mode(r);
+    if(reg < 8){
+        (r + reg) -> reg_data = value;
+    }else if(reg < 13){
+        (r + 8 + (reg-8)*2) -> reg_data = value;
+    }else if(reg < 15){
+        (r + 18 + (reg-13)*6) -> reg_data = value;      
+    }else{
+        (r + 30) -> reg_data = value;
+    }
 }
 
 void write_cpsr(registers r, uint32_t value) {
+    (r + 31) -> reg_data = value;
 }
 
 void write_spsr(registers r, uint32_t value) {
+    (r + 32) -> reg_data = value;
 }
