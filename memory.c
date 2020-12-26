b48c42527f6a2e8fb4a1a3badeb2fe0fb13dad81
SaviorSeva <49302415+SaviorSeva@users.noreply.github.com>
2020-12-26 16:00:00 +0100

    Memory.c fini et vérifier

--- memory.c
+++ memory.c
@@ -1,64 +1,147 @@
 /*
-Armator - simulateur de jeu d'instruction ARMv5T à but pédagogique
+Armator - simulateur de jeu d'instruction ARMv5T ï¿½ but pï¿½dagogique
 Copyright (C) 2011 Guillaume Huard
 Ce programme est libre, vous pouvez le redistribuer et/ou le modifier selon les
-termes de la Licence Publique Générale GNU publiée par la Free Software
-Foundation (version 2 ou bien toute autre version ultérieure choisie par vous).
+termes de la Licence Publique Gï¿½nï¿½rale GNU publiï¿½e par la Free Software
+Foundation (version 2 ou bien toute autre version ultï¿½rieure choisie par vous).
 
-Ce programme est distribué car potentiellement utile, mais SANS AUCUNE
+Ce programme est distribuï¿½ car potentiellement utile, mais SANS AUCUNE
 GARANTIE, ni explicite ni implicite, y compris les garanties de
-commercialisation ou d'adaptation dans un but spécifique. Reportez-vous à la
-Licence Publique Générale GNU pour plus de détails.
+commercialisation ou d'adaptation dans un but spï¿½cifique. Reportez-vous ï¿½ la
+Licence Publique Gï¿½nï¿½rale GNU pour plus de dï¿½tails.
 
-Vous devez avoir reçu une copie de la Licence Publique Générale GNU en même
-temps que ce programme ; si ce n'est pas le cas, écrivez à la Free Software
+Vous devez avoir reï¿½u une copie de la Licence Publique Gï¿½nï¿½rale GNU en mï¿½me
+temps que ce programme ; si ce n'est pas le cas, ï¿½crivez ï¿½ la Free Software
 Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307,
-États-Unis.
+ï¿½tats-Unis.
 
 Contact: Guillaume.Huard@imag.fr
-	 Bâtiment IMAG
+	 Bï¿½timent IMAG
 	 700 avenue centrale, domaine universitaire
-	 38401 Saint Martin d'Hères
+	 38401 Saint Martin d'Hï¿½res
 */
 #include <stdlib.h>
 #include "memory.h"
 #include "util.h"
 
 struct memory_data {
+    uint32_t * mem_data;
+    uint8_t endianess;
+    size_t size;
 };
 
 memory memory_create(size_t size, int is_big_endian) {
-    memory mem=NULL;
-    return mem;
+    uint32_t * mdata = calloc(size, sizeof(uint32_t));
+    memory m = malloc(size*(sizeof(uint32_t)) + sizeof(uint8_t) + sizeof(size_t));
+    m->mem_data = mdata;
+    m->endianess = is_big_endian;
+    m->size = size;
+    return m;
 }
 
 size_t memory_get_size(memory mem) {
-    return 0;
+    return mem->size;
 }
 
 void memory_destroy(memory mem) {
+    free(mem);
 }
 
 int memory_read_byte(memory mem, uint32_t address, uint8_t *value) {
-    return -1;
+    *value = *(mem->mem_data + address);
+    return 1;
 }
 
+// May have to verify address % 2 == 0
 int memory_read_half(memory mem, uint32_t address, uint16_t *value) {
-    return -1;
+    uint8_t res1 = 0, res2 = 0;
+    switch (mem->endianess){
+        case 1 :
+            //big endian
+            memory_read_byte(mem, address, &res1);
+            memory_read_byte(mem, address + 1, &res2);
+            *value = (res1 << 8) + res2;
+            return 1;
+            break;
+        default :
+            //Little endian
+            memory_read_byte(mem, address, &res1);
+            memory_read_byte(mem, address + 1, &res2);
+            *value = (res2 << 8) + res1;
+            return 1;
+            break;
+    }
 }
 
 int memory_read_word(memory mem, uint32_t address, uint32_t *value) {
-    return -1;
+    uint8_t res1 = 0, res2 = 0, res3 = 0, res4 = 0;
+    switch (mem->endianess){
+        case 1 :
+            //big endian
+            memory_read_byte(mem, address, &res1);
+            memory_read_byte(mem, address + 1, &res2);
+            memory_read_byte(mem, address + 2, &res3);
+            memory_read_byte(mem, address + 3, &res4);
+            *value = (res1 << 24) + (res2 << 16) + (res3 << 8) + res4;
+            return 1;
+            break;
+        default :
+            //Little endian
+            memory_read_byte(mem, address, &res1);
+            memory_read_byte(mem, address + 1, &res2);
+            memory_read_byte(mem, address + 2, &res3);
+            memory_read_byte(mem, address + 3, &res4);
+            *value = (res4 << 24) + (res3 << 16) + (res2 << 8) + res1;
+            return 1;
+            break;
+    }
 }
 
 int memory_write_byte(memory mem, uint32_t address, uint8_t value) {
-    return -1;
+    *(mem->mem_data + address) = value;
+    return 1;
 }
 
 int memory_write_half(memory mem, uint32_t address, uint16_t value) {
-    return -1;
+    uint8_t res1 = value & 0xFF;
+    uint8_t res2 = (value >> 8) & 0xFF;
+    switch (mem->endianess){
+        case 1:
+        //Big Endian
+            memory_write_byte(mem, address, res2);
+            memory_write_byte(mem, address + 1, res1);
+            return 1;
+            break;
+        default:
+        //Little Endian
+            memory_write_byte(mem, address, res1);
+            memory_write_byte(mem, address + 1, res2);
+            return 1;
+            break;
+    }
 }
 
 int memory_write_word(memory mem, uint32_t address, uint32_t value) {
-    return -1;
+    uint8_t res1 = value & 0xFF;
+    uint8_t res2 = (value >> 8) & 0xFF;
+    uint8_t res3 = (value >> 16) & 0xFF;
+    uint8_t res4 = (value >> 24) & 0xFF;
+    switch (mem->endianess){
+        case 1:
+        //Big Endian
+            memory_write_byte(mem, address, res4);
+            memory_write_byte(mem, address + 1, res3);
+            memory_write_byte(mem, address + 2, res2);
+            memory_write_byte(mem, address + 3, res1);
+            return 1;
+            break;
+        default:
+        //Little Endian
+            memory_write_byte(mem, address, res1);
+            memory_write_byte(mem, address + 1, res2);
+            memory_write_byte(mem, address + 2, res3);
+            memory_write_byte(mem, address + 3, res4);
+            return 1;
+            break;
+    }
 }
