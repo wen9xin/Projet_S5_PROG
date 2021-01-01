3891af4139054bfd6a05c334f4ecf11ad507095b
Yucheng XIAO <saviorseva@gmai.com>
2021-01-01 17:08:04 +0100

    Adressing mode 1 for data processing completed

--- arm_data_processing.c
+++ arm_data_processing.c
@@ -1,24 +1,24 @@
 /*
-Armator - simulateur de jeu d'instruction ARMv5T à but pédagogique
+Armator - simulateur de jeu d'instruction ARMv5T ï¿½ but pï¿½dagogique
 Copyright (C) 2011 Guillaume Huard
 Ce programme est libre, vous pouvez le redistribuer et/ou le modifier selon les
-termes de la Licence Publique Générale GNU publiée par la Free Software
-Foundation (version 2 ou bien toute autre version ultérieure choisie par vous).
+termes de la Licence Publique Gï¿½nï¿½rale GNU publiï¿½e par la Free Software
+Foundation (version 2 ou bien toute autre version ultï¿½rieure choisie par vous).
 
-Ce programme est distribué car potentiellement utile, mais SANS AUCUNE
+Ce programme est distribuï¿½ car potentiellement utile, mais SANS AUCUNE
 GARANTIE, ni explicite ni implicite, y compris les garanties de
-commercialisation ou d'adaptation dans un but spécifique. Reportez-vous à la
-Licence Publique Générale GNU pour plus de détails.
+commercialisation ou d'adaptation dans un but spï¿½cifique. Reportez-vous ï¿½ la
+Licence Publique Gï¿½nï¿½rale GNU pour plus de dï¿½tails.
 
-Vous devez avoir reçu une copie de la Licence Publique Générale GNU en même
-temps que ce programme ; si ce n'est pas le cas, écrivez à la Free Software
+Vous devez avoir reï¿½u une copie de la Licence Publique Gï¿½nï¿½rale GNU en mï¿½me
+temps que ce programme ; si ce n'est pas le cas, ï¿½crivez ï¿½ la Free Software
 Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307,
-États-Unis.
+ï¿½tats-Unis.
 
 Contact: Guillaume.Huard@imag.fr
-	 Bâtiment IMAG
+	 Bï¿½timent IMAG
 	 700 avenue centrale, domaine universitaire
-	 38401 Saint Martin d'Hères
+	 38401 Saint Martin d'Hï¿½res
 */
 #include "arm_data_processing.h"
 #include "arm_exception.h"
@@ -26,6 +26,257 @@ Contact: Guillaume.Huard@imag.fr
 #include "arm_branch_other.h"
 #include "util.h"
 #include "debug.h"
+
+int get_cflag(arm_core p){
+	return get_bit(arm_read_cpsr(p), 29);
+}
+
+uint32_t rotateRight(uint32_t x, uint32_t n) {
+    uint32_t shifted = x >> n;
+    uint32_t rot_bits = x << (32-n);
+    uint32_t combined = shifted | rot_bits;
+
+    return combined;
+}
+
+uint32_t arithmetic_shift_right(uint32_t x, uint32_t n){
+	uint32_t res = 0;
+	if(get_bit(x, 31) == 0) res = x >> n;
+	else{
+		uint32_t mask = 0xFFFFFFFF;
+		mask = ~(mask >> n);
+		res = (x >> n) & mask;
+	}
+	return res;
+}
+
+/*  
+	Functions for Addressing mode 1
+	return an 64 bit unsigned int where:
+ 	first 32 bits is shifter_operand,
+ 	second 32 bits is shifter_carry_out.
+*/
+
+uint64_t packing_shifter(uint32_t shifter_operand, uint32_t shifter_carry_out){
+	uint64_t res = (shifter_operand << 32) | shifter_carry_out;
+	return res;
+}
+
+uint32_t get_shifter_operand(uint64_t res){
+	return (res >> 32) & 0xFFFFFFFF;
+}
+
+uint32_t get_shifter_carry_out(uint64_t res){
+	return res & 0xFFFFFFFF;
+}
+
+// Data-processing operands - Immediate
+uint64_t imm(arm_core p, uint32_t ins){
+	uint32_t immed_8 = get_bits(ins, 7, 0);
+	uint32_t rotate_imm = get_bits(ins, 11, 8);
+	uint32_t shifter_operand = rotateRight(immed_8, rotate_imm);
+	uint32_t shifter_carry_out = 0;
+	if(rotate_imm == 0) shifter_carry_out = get_cflag(p);
+	else shifter_carry_out = get_bit(shifter_operand, 31);
+	uint64_t res = packing_shifter(shifter_operand, shifter_carry_out);
+	return res;
+}
+
+// Data-processing operands - Register
+uint64_t reg(arm_core p, uint32_t ins){
+	uint32_t shifter_operand = arm_read_register(p, get_bits(ins, 3, 0));
+	uint32_t shifter_carry_out = get_cflag(p);
+	uint64_t res = packing_shifter(shifter_operand, shifter_carry_out);
+	return res;
+}
+// Data-processing operands - Logical shift left by immediate
+uint64_t lsl_imm(arm_core p, uint32_t ins){
+	uint32_t rm_data = arm_read_register(p, get_bits(ins, 3, 0));
+	uint8_t shift_imm = get_bits(ins, 11, 7);
+	uint32_t shifter_operand = 0;
+	uint32_t shifter_carry_out = 0;
+	if(shift_imm == 0){
+		shifter_operand = rm_data;
+		shifter_carry_out = get_cflag(p);
+	}else{
+		shifter_operand = rm_data << shift_imm;
+		shifter_carry_out = get_bit(rm_data, 32-shift_imm);
+	}
+	uint64_t res = packing_shifter(shifter_operand, shifter_carry_out);
+	return res;
+}
+
+// Data-processing operands - Logical shift left by register
+uint64_t lsl_reg(arm_core p, uint32_t ins){
+	uint8_t rs_7_0_data = arm_read_register(p, get_bits(ins, 11, 8)) & 0xFF;
+	uint32_t rm_data = arm_read_register(p, get_bits(ins, 3, 0));
+	uint32_t shifter_operand = 0;
+	uint32_t shifter_carry_out = 0;
+	if(rs_7_0_data == 0){
+		shifter_operand = rm_data;
+		shifter_carry_out = get_cflag(p);
+	}else if (rs_7_0_data < 32){
+		shifter_operand = rm_data << rs_7_0_data;
+		shifter_carry_out = get_bit(rm_data, 32 - rs_7_0_data);
+	}else if(rs_7_0_data == 32){
+		shifter_operand = 0;
+		shifter_carry_out = get_bit(rm_data, 0);
+	}else{
+		shifter_operand = 0;
+		shifter_carry_out = 0;
+	}
+	uint64_t res = packing_shifter(shifter_operand, shifter_carry_out);
+	return res;
+}
+
+// Data-processing operands - Logical shift right by immediate
+uint64_t lsr_imm(arm_core p, uint32_t ins){
+	uint32_t rm_data = arm_read_register(p, get_bits(ins, 3, 0));
+	uint8_t shift_imm = get_bits(ins, 11, 7);
+	uint32_t shifter_operand = 0;
+	uint32_t shifter_carry_out = 0;
+	if(shift_imm == 0){
+		shifter_operand = 0;
+		shifter_carry_out = get_bit(rm_data, 31);
+	}else{
+		shifter_operand = rm_data >> shift_imm;
+		shifter_carry_out = get_bit(rm_data, shift_imm - 1);
+	}
+	uint64_t res = packing_shifter(shifter_operand, shifter_carry_out);
+	return res;
+}
+
+// Data-processing operands - Logical shift right by register
+uint64_t lsr_reg(arm_core p, uint32_t ins){
+	uint8_t rs_7_0_data = arm_read_register(p, get_bits(ins, 11, 8)) & 0xFF;
+	uint32_t rm_data = arm_read_register(p, get_bits(ins, 3, 0));
+	uint32_t shifter_operand = 0;
+	uint32_t shifter_carry_out = 0;
+	if(rs_7_0_data == 0){
+		shifter_operand = rm_data;
+		shifter_carry_out = get_cflag(p);
+	}else if (rs_7_0_data < 32){
+		shifter_operand = rm_data >> rs_7_0_data;
+		shifter_carry_out = get_bit(rm_data, rs_7_0_data - 1);
+	}else if(rs_7_0_data == 32){
+		shifter_operand = 0;
+		shifter_carry_out = get_bit(rm_data, 31);
+	}else{
+		shifter_operand = 0;
+		shifter_carry_out = 0;
+	}
+	uint64_t res = packing_shifter(shifter_operand, shifter_carry_out);
+	return res;
+}
+
+// Data-processing operands - Arithmetic shift right by immediate
+uint64_t asr_imm(arm_core p, uint32_t ins){
+	uint32_t rm_data = arm_read_register(p, get_bits(ins, 3, 0));
+	uint8_t shift_imm = get_bits(ins, 11, 7);
+	uint32_t shifter_operand = 0;
+	uint32_t shifter_carry_out = 0;
+	if(shift_imm == 0){
+		if(get_bit(rm_data, 31) == 0) shifter_operand = 0;
+		else shifter_operand = 0xFFFFFFFF;
+		shifter_carry_out = get_bit(rm_data, 31);	
+	}else{		shifter_operand = arithmetic_shift_right(rm_data, shift_imm);
+		shifter_carry_out = get_bit(rm_data, shift_imm - 1);
+	}
+	uint64_t res = packing_shifter(shifter_operand, shifter_carry_out);
+	return res;
+}
+
+// Data-processing operands - Arithmetic shift right by register
+uint64_t asr_reg(arm_core p, uint32_t ins){
+	uint8_t rs_7_0_data = arm_read_register(p, get_bits(ins, 11, 8)) & 0xFF;
+	uint32_t rm_data = arm_read_register(p, get_bits(ins, 3, 0));
+	uint32_t shifter_operand = 0;
+	uint32_t shifter_carry_out = 0;
+	if(rs_7_0_data == 0){
+		shifter_operand = rm_data;
+		shifter_carry_out = get_cflag(p);
+	}else if (rs_7_0_data < 32){
+		shifter_operand = arithmetic_shift_right(rm_data, rs_7_0_data);
+		shifter_carry_out = get_bit(rm_data, rs_7_0_data - 1);
+	}else if(rs_7_0_data == 32){
+		shifter_operand = 0;
+		shifter_carry_out = get_bit(rm_data, 31);
+	}else{
+		shifter_operand = 0xFFFFFFFF;
+		shifter_carry_out = 0;
+	}
+	uint64_t res = packing_shifter(shifter_operand, shifter_carry_out);
+	return res;
+}
+
+// Data-processing operands - Rotate right with extend
+uint64_t rrx(arm_core p, uint32_t ins){
+	uint32_t rm_data = arm_read_register(p, get_bits(ins, 3, 0));
+	uint32_t shifter_operand = (get_cflag(p) << 31) | (rm_data > 1);
+	uint32_t shifter_carry_out = get_bit(rm_data, 0);
+	uint64_t res = packing_shifter(shifter_operand, shifter_carry_out);
+	return res;
+}
+
+// Data-processing operands - Rotate right by immediate
+uint64_t ror_imm(arm_core p, uint32_t ins){
+	uint32_t rm_data = arm_read_register(p, get_bits(ins, 3, 0));
+	uint8_t shift_imm = get_bits(ins, 11, 7);
+	uint32_t shifter_operand = 0;
+	uint32_t shifter_carry_out = 0;
+	if(shift_imm == 0) return rrx(p, ins);	
+	else{
+		shifter_operand = rotateRight(rm_data, shift_imm);
+		shifter_carry_out = get_bit(rm_data, shift_imm - 1);
+	}
+	uint64_t res = packing_shifter(shifter_operand, shifter_carry_out);
+	return res;
+}
+
+// Data-processing operands - Rotate right by register
+uint64_t ror_reg(arm_core p, uint32_t ins){
+	uint8_t rs_7_0_data = arm_read_register(p, get_bits(ins, 11, 8)) & 0xFF;
+	uint8_t rs_4_0_data = get_bits(rs_7_0_data, 4, 0);
+	uint32_t rm_data = arm_read_register(p, get_bits(ins, 3, 0));
+	uint32_t shifter_operand = 0;
+	uint32_t shifter_carry_out = 0;
+	if(rs_7_0_data == 0){
+		shifter_operand = rm_data;
+		shifter_carry_out = get_cflag(p);
+	}else if (rs_4_0_data == 0){
+		shifter_operand = rm_data;
+		shifter_carry_out = get_bit(rm_data, 31);
+	}else{
+		shifter_operand = rotateRight(rm_data, rs_4_0_data);
+		shifter_carry_out = get_bit(rm_data, rs_4_0_data - 1);
+	}
+	uint64_t res = packing_shifter(shifter_operand, shifter_carry_out);
+	return res;
+}
+
+uint64_t get_operande_and_carry_out(arm_core p, uint32_t ins) {
+	uint64_t res = 0;
+	if(get_bits(ins, 27, 25) == 1) res = imm(p, ins);
+	else if(get_bits(ins, 27, 25) == 0){
+		if(get_bit(ins, 4) == 0){	// Immediate shifts / RRX
+			if(get_bits(ins, 6, 4) == 0) res = lsl_imm(p, ins);
+			else if(get_bits(ins, 6, 4) == 2) res = lsr_imm(p, ins);
+			else if(get_bits(ins, 6, 4) == 4) res = asr_imm(p, ins);
+			else if(get_bits(ins, 6, 4) == 6) res = ror_imm(p, ins);
+			else if(get_bits(ins, 11, 4) == 6) res = rrx(p, ins);
+			else return 0;
+
+		}else{	// register shifts
+			if(get_bits(ins, 11, 4) == 0) res = reg(p, ins);
+			else if(get_bits(ins, 7, 4) == 1) res = lsl_reg(p, ins);
+			else if(get_bits(ins, 7, 4) == 3) res = lsr_reg(p, ins);
+			else if(get_bits(ins, 7, 4) == 5) res = asr_reg(p, ins);
+			else if(get_bits(ins, 7, 4) == 7) res = ror_reg(p, ins);
+			else return 0;
+		}
+	}
+	return res;
+}
 
 /* Decoding functions for different classes of instructions */
 int arm_data_processing_shift(arm_core p, uint32_t ins) {
